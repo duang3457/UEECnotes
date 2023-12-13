2c580c118121c16e3537a82342056e277ccdfe1f
yang <1837827317@qq.com>
2023-12-13 22:27:09 +1100

    version0.1.0

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True, unique=True)
+    post_name = models.CharField(max_length=100)
+    post_content = models.TextField()
+
+    def __str__(self):
+       return f'post_name: {self.post_name}'
+
--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,HttpResponse
+from forums.models import Post
 
 # Create your views here.
 
 def forums(request):
-    return render(request, 'forums.html')
+    if request.method == 'POST':
+        # 处理表单提交
+
+        title = request.POST.get('post-title')
+        content = request.POST.get('post-content')
+
+        # 创建并保存新的Post对象
+        Post.objects.create(post_name=title, post_content=content)
+
+
+        # 可以根据需要重定向到成功页面或其他页面
+        return redirect("forums:forums")
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(request, 'forums.html', {'posts': posts})
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def ab(request):
+    return render(request, 'notes/ab.html')
