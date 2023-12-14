752665e4e343bb43184d6325943d33a608df91ed
yang <1837827317@qq.com>
2023-12-14 22:02:48 +1100

    version0.1.1

--- UEECnotes/settings.py
+++ UEECnotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-uja6j5_0lkz!^*v-+a8c1al!#m_i+hx5cae^us)2!i-5%t7ti3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ueec.yang3457.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -118,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
